9139fd4b3aae80d40d11dd5c48b55db5c637a828
mariachtz <maria.chatzigeorgiou@wur.nl>
2025-04-04 11:39:52 +0200

    step 1 and 2

---
+++ art.py
@@ -0,0 +1,28 @@
+
+
+
+
+# Import relevant functions from the module 'drawing'
+# Create constant COLOUR for the colour of the drawing
+# Create constant PATH for where to save the SVG drawing
+# Save radial lines into a file
+
+from drawing import make_drawing_radial_lines
+from drawing import make_drawing_cross
+from drawing import save_in_file
+from drawing import draw_line
+COLOUR = "blue"
+
+# Create the SVG content
+svg_content = make_drawing_cross(draw_line, COLOUR)
+
+# # Print to check if it looks okay
+print(svg_content)
+
+# Save to file
+save_in_file("art.svg", svg_content)
+
+
+radial_svg = make_drawing_radial_lines(COLOUR)
+print(radial_svg)
+save_in_file("radial.svg", radial_svg)
---
+++ drawing.py
@@ -0,0 +1,42 @@
+"""Collection of SVG drawing functions"""
+
+def draw_line(x, y, x2, y2, colour):
+    return f'<line x1="{x}" y1="{y}" x2="{x2}" y2="{y2}" stroke="{colour}" />\n'
+
+
+
+
+def make_drawing_radial_lines(colour= "black", width=100, height=100, granularity=5):
+    svg = f'<svg viewBox="0 0 {width} {height}" width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">\n'
+
+    x = granularity                         # Left with margin
+    y = granularity                         # Top with margin
+    x2 = width - granularity                # Right with margin
+    y2 = height - granularity               # Bottom with margin
+
+    # Vertical movement -  y and y2
+    for i in range(0, height, granularity):
+        svg += draw_line(x, y + i, x2, y2 - i, colour)
+
+    # Horizontal movement - x and x2
+    for i in range(0, width, granularity):
+        svg += draw_line(x + i, y, x2 - i, y2, colour)
+
+    svg += '</svg>'
+    return svg
+
+
+
+def save_in_file(filename, content):
+    with open(filename, "w") as file:
+        file.write(content)
+
+
+
+
+def make_drawing_cross(draw_line_function, colour):
+    svg = '<svg viewBox="0 0 100 100" width="100" height="100" xmlns="http://www.w3.org/2000/svg">\n'
+    svg += draw_line_function(10, 10, 90, 90, colour)
+    svg += draw_line_function(10, 90, 90, 10, colour)
+    svg += '</svg>'
+    return svg
